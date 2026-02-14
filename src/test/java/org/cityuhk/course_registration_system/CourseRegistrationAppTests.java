2b8a205de4e40d5b4c32674146c2e830e3698e14
Your Name <you@example.com>
2026-02-14 22:27:38 +0800

    init

---
+++ src/test/java/org/cityuhk/course_registration_system/CourseRegistrationAppTests.java
@@ -0,0 +1,13 @@
+package org.cityuhk.course_registration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegistrationAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
