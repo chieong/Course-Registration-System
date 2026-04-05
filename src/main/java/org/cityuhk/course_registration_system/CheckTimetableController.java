ea4de249776e6eab5eccbe0ce55c66bde1d916a4
9rtoria <sprsunnykirito@gmail.com>
2026-04-05 16:24:06 +0800

    timetable page added

---
+++ src/main/java/org/cityuhk/course_registration_system/CheckTimetableController.java
@@ -0,0 +1,14 @@
+package org.cityuhk.course_registration_system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CheckTimetableController {
+
+    @GetMapping("/Timetable")
+    public String checkTimetable(Model model) {
+        return "Timetable"; // resolves to templates/check-timetable.html
+    }
+}
