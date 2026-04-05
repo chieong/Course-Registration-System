4a3a7e2257c52fe0c7373a378f2844fd84adc9e1
CHAN Chun Hin <chchan2494-c@my.cityu.edu.hk>
2026-04-05 15:54:22 +0800

    add viewmasterclass schedule, change css, login page

---
+++ src/main/java/org/cityuhk/course_registration_system/ViewMasterClassScheduleController.java
@@ -0,0 +1,15 @@
+package org.cityuhk.course_registration_system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewMasterClassScheduleController {
+
+    @GetMapping("/ViewMasterClassSchedule") 
+    public String ViewMasterClassSchedule(Model model) { 
+        return "ViewMasterClassSchedule";
+    }
+
+}
