e6fa8958db62b763802e71e2ca089fb6c4716e45
9rtoria <sprsunnykirito@gmail.com>
2026-04-05 21:48:10 +0800

    manage class page added

---
+++ src/main/java/org/cityuhk/course_registration_system/ManageClassesController.java
@@ -0,0 +1,14 @@
+package org.cityuhk.course_registration_system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ManageClassesController {
+
+    @GetMapping("/manageclass")
+    public String manageClasses(Model model) {
+        return "manageclass"; // resolves to templates/manage-classes.html
+    }
+}
