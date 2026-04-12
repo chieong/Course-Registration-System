35cc5cc9715ed432861d982bfaef885df1474ae7
chieong <232149274+chieong@users.noreply.github.com>
2026-04-12 15:26:25 +0800

    fix academic record
    no idea how it broke
    also it compiles and runs now

--- src/main/java/org/cityuhk/CourseRegistrationSystem/Model/AcademicRecord.java
+++ src/main/java/org/cityuhk/CourseRegistrationSystem/Model/AcademicRecord.java
@@ -1,27 +1,28 @@
 package org.cityuhk.CourseRegistrationSystem.Model;
 
+
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"student_id", "course_id"}))
+public class AcademicRecord {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int recordId;
     // private int completedCredit;
-    private int completedCredit;
-    private float GPA;
-    private String standing;
-    @OneToMany 
+    // private float GPA;
+    // private String standing;
     public AcademicRecord() {}
 
-    public AcademicRecord(int recordId, int completedCredit, float GPA, String standing) {
-        this.recordId = recordId;
-        this.completedCredit = completedCredit;
-        this.GPA = GPA;
-        this.standing = standing;
-    }
+    // public AcademicRecord(int recordId, int completedCredit, float GPA, String standing) {
+    //     this.recordId = recordId;
+    //     this.completedCredit = completedCredit;
+    //     this.GPA = GPA;
+    //     this.standing = standing;
+    // }
 }
 
