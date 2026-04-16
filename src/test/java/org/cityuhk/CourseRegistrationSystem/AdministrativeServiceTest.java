c66b40d98c6f907fab24281d8ba9140a0fd95918
chieong <232149274+chieong@users.noreply.github.com>
2026-04-16 08:22:20 +0800

    fix: admin serv test package

--- src/test/java/org/cityuhk/CourseRegistrationSystem/AdministrativeServiceTest.java
+++ src/test/java/org/cityuhk/CourseRegistrationSystem/AdministrativeServiceTest.java
@@ -1,4 +1,4 @@
-package org.cityuhk.CourseRegistrationSystem.Service;
+package org.cityuhk.CourseRegistrationSystem;
 
 import org.cityuhk.CourseRegistrationSystem.Controller.dto.AdminCourseRequest;
 import org.cityuhk.CourseRegistrationSystem.Controller.dto.AdminUserRequest;
@@ -6,6 +6,7 @@ import org.cityuhk.CourseRegistrationSystem.Model.Admin;
 import org.cityuhk.CourseRegistrationSystem.Model.Course;
 import org.cityuhk.CourseRegistrationSystem.Repository.AdminRepository;
 import org.cityuhk.CourseRegistrationSystem.Repository.CourseRepository;
+import org.cityuhk.CourseRegistrationSystem.Service.AdministrativeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
